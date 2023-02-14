c6820832b2cd758dfebfcfc5c321fa6d8d349e08
Daniel Takahashi <daniel.takahashi@objective.com.br>
2023-02-14 11:53:21 -0300

    first commit

---
+++ src/Sample.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sample\Php;
+
+final class Sample 
+{
+    function __construct()
+    {
+        phpinfo();
+    }
+}
---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use Sample\Php\Sample;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+new Sample;
